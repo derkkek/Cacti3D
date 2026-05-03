0b2c613ac3e19b1113611821c70d3e2b50da18d1
Eren <erenhalil2010@hotmail.com>
2026-05-03 12:50:20 +0300

    sphere-sphere dynamic intersection test

--- src/Intersections.cpp
+++ src/Intersections.cpp
@@ -62,7 +62,7 @@ namespace Cacti
 		// TODO: either populate world points in here by calling local space to world space func or in resolution. Currently it's being populated only for spheres.
 		return false;
 	}
-	bool Intersections::RayTraceCollidedWithSphere(Vec3& rayOrigin, Vec3& rayDir, Vec3& sphereCenter, float& sphereRadius, float& t1, float& t2)
+	bool Intersections::RayTraceCollidedWithSphere(const Vec3& rayOrigin, const Vec3& rayDir, const Vec3& sphereCenter, float& sphereRadius, float& t1, float& t2)
 	{
 		float a = rayDir.Dot(rayDir);
 		float b = 2 * rayDir.Dot(rayOrigin - sphereCenter);
@@ -77,6 +77,64 @@ namespace Cacti
 
 		t1 = (-b - sqrtf(discriminant)) / (2 * a);
 		t2 = (-b + sqrtf(discriminant)) / (2 * a);
+
+		return true;
+	}
+
+	bool Intersections::SphereSphereDynamic(Sphere* sphereA, Sphere* sphereB, const Vec3& vA, const Vec3& vB, const Vec3& aPos, const Vec3& bPos, const float dt, float t0, float t1, float toi, Vec3 collisionPointOnA, Vec3 collisionPointOnB)
+	{
+		const Vec3 relV = vA - vB; // let's say B is static.
+		const Vec3 rayOrigin = aPos;
+		const Vec3 rayEnd = aPos + relV * dt;
+		const Vec3 rayVector = rayEnd - rayOrigin;
+
+		float radiusSum = sphereA->radius + sphereB->radius + 0.001f;
+
+
+		if (rayVector.GetLengthSqr() <= 0.001f * 0.001f)
+		{
+			//we're so close are we colliding right now?
+
+			const Vec3 ab = bPos - aPos;
+			if (ab.GetLengthSqr() > radiusSum * radiusSum)
+			{
+				return false;
+			}
+		}
+
+		else if (!RayTraceCollidedWithSphere(rayOrigin, rayVector, bPos, radiusSum, t0, t1))
+		{
+			return false;
+		}
+
+		//change our scale from [0, 1] to [0, dt], (dt is the time of the final position of the ray.)
+		//Because of this we can sort collision times and handle collisions chronogically otherwise we could miss prior collisions.
+		//Also while calculating new positions we will use dt for our toi. Using [0,1] scale is useless.
+		t0 *= dt;
+		t1 *= dt;
+
+		if (t1 < 0)
+		{
+			//Collision is in the past, no future collisions in this frame.
+			return false;
+		}
+
+		//earliest time of impact
+		toi = (t0 < 0.0f) ? 0.0f : t0;
+
+		if (toi > dt)
+		{
+			//earliest collision is not in this frame, it's in future.
+			return false;
+		}
+
+		const Vec3 newPositionA = aPos + vA * toi;
+		const Vec3 newPositionB = bPos + vB * toi;
+
+		const Vec3 collisionNormal = (newPositionA - newPositionB).Normalize();
+
+		collisionPointOnA = newPositionA + collisionNormal * sphereA->radius;
+		collisionPointOnB = newPositionB - collisionNormal * sphereB->radius;
 
 		return true;
 	}
--- src/Intersections.h
+++ src/Intersections.h
@@ -10,7 +10,8 @@ namespace Cacti
 		~Intersections() = default;
 
 		static bool Intersect(Body& bodyA, Body& bodyB, Contact& contact);
-		static bool RayTraceCollidedWithSphere(Vec3& rayOrigin, Vec3& rayDir, Vec3& sphereCenter, float& sphereRadius, float& t1, float& t2);
+		static bool RayTraceCollidedWithSphere(const Vec3& rayOrigin, const Vec3& rayDir, const Vec3& sphereCenter, float& sphereRadius, float& t1, float& t2);
+		bool SphereSphereDynamic(Sphere* sphereA, Sphere* sphereB, const Vec3& vA, const Vec3& vB, const Vec3& aPos, const Vec3& bPos, const float dt, float t0, float t1, float toi, Vec3 collisionPointOnA, Vec3 collisionPointOnB);
 	private:
 
 	};
